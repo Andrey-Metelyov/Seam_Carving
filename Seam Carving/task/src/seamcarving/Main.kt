974a81104a8f64b6b2fe13803c9fe83157cf72bc
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-08-18 01:15:32 +0900

    Stage 2/6: Negative photo

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -5,7 +5,37 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-fun main() {
+val params = mutableMapOf<String, String>()
+
+fun main(args: Array<String>) {
+    readParams(args)
+    val input = params["-in"]
+    val output = params["-out"]
+
+    val image = ImageIO.read(File(input))
+    for (i in 0 until image.width)
+        for (j in 0 until image.height)
+            image.setRGB(i, j, negative(image.getRGB(i, j)))
+
+    ImageIO.write(image, "png", File(output))
+}
+
+fun negative(rgb: Int): Int {
+    val color = Color(rgb)
+    return Color(255 - color.red, 255 - color.green, 255 - color.blue).rgb
+}
+
+fun readParams(args: Array<String>) {
+    for (i in 0..args.lastIndex) {
+        if (args[i].startsWith("-")) {
+            params[args[i]] = args[i + 1]
+        } else {
+            continue
+        }
+    }
+}
+
+private fun makeXImage() {
     println("Enter rectangle width:")
     val width = readLine()!!.toInt()
     println("Enter rectangle height:")
