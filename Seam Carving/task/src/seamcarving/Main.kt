86e2b790a0c6cde6f6b89c79e02bf17430bb18fe
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-08-18 02:07:16 +0900

    Stage 3/6: Look at energy

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -3,6 +3,8 @@ package seamcarving
 import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
+import java.lang.Integer.max
+import java.lang.Integer.min
 import javax.imageio.ImageIO
 
 val params = mutableMapOf<String, String>()
@@ -12,6 +14,49 @@ fun main(args: Array<String>) {
     val input = params["-in"]
     val output = params["-out"]
 
+    makeEnergyImage(input, output)
+}
+
+private fun makeEnergyImage(input: String?, output: String?) {
+    val image = ImageIO.read(File(input))
+    val energy = Array(image.width) { Array(image.height) { 0.0 } }
+    System.err.println("image ${image.width} x ${image.height}")
+    var maxEnergy = 0.0
+    for (i in 0 until image.width) {
+        for (j in 0 until image.height) {
+            val left = Color(image.getRGB(max(i - 1, 0), j))
+            val right = Color(image.getRGB(min(i + 1, image.width - 1), j))
+            val top = Color(image.getRGB(i, max(j - 1, 0)))
+            val bottom = Color(image.getRGB(i, min(j + 1, image.height - 1)))
+
+            val dx = (left.red - right.red) * (left.red - right.red)
+            +(left.green - right.green) * (left.green - right.green)
+            +(left.blue - right.blue) * (left.blue - right.blue)
+            val dy = (bottom.red - top.red) * (bottom.red - top.red)
+            +(bottom.green - top.green) * (bottom.green - top.green)
+            +(bottom.blue - top.blue) * (bottom.blue - top.blue)
+            energy[i][j] = kotlin.math.sqrt((dx + dy).toDouble())
+            System.err.println(
+                "E($i,$j)=${energy[i][j]} " +
+                        "left: ${max(i - 1, 0)},$j" +
+                        " right: ${min(i + 1, image.width - 1)},$j" +
+                        " top: $i,${max(j - 1, 0)}" +
+                        " bottom: $i,${min(j + 1, image.height - 1)}"
+            )
+            if (energy[i][j] > maxEnergy) {
+                maxEnergy = energy[i][j]
+            }
+        }
+    }
+    for (i in 0 until image.width)
+        for (j in 0 until image.height)
+            image.setRGB(i, j, (255.0 * energy[i][j] / maxEnergy).toInt())
+
+    ImageIO.write(image, "png", File(output))
+}
+
+
+private fun makeNegativeImage(input: String?, output: String?) {
     val image = ImageIO.read(File(input))
     for (i in 0 until image.width)
         for (j in 0 until image.height)
